60259a34793d36a1bb3ef419c665de07d227b27e
Guy <gpurssell@gmail.com>
2019-01-07 11:27:01 +0000

    playIcon Id

--- static/js/gwp.js
+++ static/js/gwp.js
@@ -1,16 +1,16 @@
-  var width = window.innerWidth;
-  var playIcon = document.querySelector('#playIcon');
-  var info = document.getElementById('information');
-  var intro = document.getElementById('introduction');
+var width = window.innerWidth;
+var playIcon = document.getElementById('#playIcon');
+var info = document.getElementById('information');
+var intro = document.getElementById('introduction');
 
-  if (width >= 768) {
-    playIcon.onmouseover = () => {
-      info.style.display = "block";
-      intro.style.display = "block";
+if (width >= 768) {
+  playIcon.onmouseover = () => {
+    info.style.display = "block";
+    intro.style.display = "block";
   };
 
-    playIcon.onmouseleave = () => {
-      info.style.display = "none";
-      intro.style.display = "none";
-    }
-  }
+  playIcon.onmouseleave = () => {
+    info.style.display = "none";
+    intro.style.display = "none";
+  };
+}
